f4123ea34bb8eaa203dac9e38ed11c45190136d2
Jeroen Beekmans <jeroen@popupcity.net>
2025-05-20 12:13:23 +0200

    Update language config

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
     locales: ['nl', 'en'],
     defaultLocale: 'nl',
     routing: {
-      prefixDefaultLocale: true,
+      prefixDefaultLocale: false,
     },
   },
 });
