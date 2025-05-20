48d16a0389aeeaa31e08bc6c0cfe86bd64a04c59
Jeroen Beekmans <jeroen@popupcity.net>
2025-05-20 12:52:10 +0200

    Delete HTML minifier (not working as expected)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import netlify from '@astrojs/netlify';
-import minifyHtml from '@jcayzac/astro-minify-html';
 
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
-  integrations: [minifyHtml()],
 
   vite: {
     plugins: [tailwindcss()],
