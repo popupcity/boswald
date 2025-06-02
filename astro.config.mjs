18e785cffc49eabd2630ac34a4756e226c2c7d97
Jeroen Beekmans <jeroen@popupcity.net>
2025-06-02 14:21:49 +0200

    Add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import netlify from '@astrojs/netlify';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://boswald.nl', // geen trailing slash!
+  site: 'https://boswald.nl',
+  integrations: [sitemap()],
   output: 'server',
   adapter: netlify(),
 
