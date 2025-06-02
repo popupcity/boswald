601aa026fad5ac0f391c3e4b1241c244c29b196f
Jeroen Beekmans <jeroen@popupcity.net>
2025-06-02 14:53:22 +0200

    Delete sitemap for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import netlify from '@astrojs/netlify';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://boswald.nl',
   output: 'server',
   adapter: netlify(),
-  integrations: [
-    sitemap({
-      filter: (page) =>
-        page !== 'https://boswald.nl/en/newsletter/subscribed/' &&
-        page !== 'https://boswald.nl/en/newsletter/unsubscribed/' &&
-        page !== 'https://boswald.nl/nieuwsbrief/aangemeld/' &&
-        page !== 'https://boswald.nl/nieuwsbrief/afgemeld/' &&
-        page !== 'https://boswald.nl/handleiding/' &&
-        page !== 'https://boswald.nl/tips/',
-      i18n: {
-        defaultLocale: 'nl',
-        locales: {
-          en: 'en-GB',
-        },
-      },
-    }),
-  ],
 
   vite: {
     plugins: [tailwindcss()],
